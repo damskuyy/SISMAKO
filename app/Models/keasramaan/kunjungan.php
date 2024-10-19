eaa1080a318431d8dd18c31f952764e9a0056ed3
fadhil1806 <fadhil8637@smk.belajar.id>
2024-10-19 22:00:12 +0700

    fixed kunjungan v2

--- app/Models/keasramaan/kunjungan.php
+++ app/Models/keasramaan/kunjungan.php
@@ -5,7 +5,7 @@ namespace App\Models\keasramaan;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class kunjungan extends Model
+class Kunjungan extends Model
 {
     use HasFactory;
     protected $table = 'visiting';
